a3d044f8f5c9065bd32c82923d5de956bb6f9be7
Enzo Sander <enzosanderr@gmail.com>
2026-09-08 20:36:19 -0300

    feat: inicializa solucion TPWinForm_EquipoL y agrega clases de dominio Articulo, Marca, Categoria e Imagen

---
+++ TPWinForm_Equipo-L/Dominio/Articulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+
+
+        //relaciones
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+
+
+        //constr
+        public Articulo()
+        {
+            Imagenes = new List<Imagen>();
+        }
+    }
+}
