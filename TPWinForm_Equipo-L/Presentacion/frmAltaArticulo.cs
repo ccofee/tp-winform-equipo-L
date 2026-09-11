95f4512e63b95e2faaf0ebeff82a34268ec49854
agustintrejo52-ux <agustintrejo52@gmail.com>
2026-09-11 11:17:23 -0300

    feat: reutiliza frmAltaArticulo en modo modificación

--- TPWinForm_Equipo-L/Presentacion/frmAltaArticulo.cs
+++ TPWinForm_Equipo-L/Presentacion/frmAltaArticulo.cs
@@ -7,9 +7,17 @@ namespace Presentacion
 {
     public partial class frmAltaArticulo : Form
     {
+        private Articulo articulo = null;
+
         public frmAltaArticulo()
         {
             InitializeComponent();
+        }
+
+        public frmAltaArticulo(Articulo articulo)
+        {
+            InitializeComponent();
+            this.articulo = articulo;
         }
         private void frmAltaArticulo_Load(object sender, EventArgs e)
         {
@@ -28,6 +36,36 @@ namespace Presentacion
             categorias.Add(new Categoria { Id = 3, Descripcion = "Accesorios" });
 
             cboCategoria.DataSource = categorias;
+            if (articulo != null)
+            {
+                Text = "Modificar artículo";
+                lblTitulo.Text = "Modificar artículo";
+                txtCodigo.Text = articulo.Codigo;
+                txtNombre.Text = articulo.Nombre;
+                txtDescripcion.Text = articulo.Descripcion;
+                txtPrecio.Text = articulo.Precio.ToString();
+
+                for (int i = 0; i < cboMarca.Items.Count; i++)
+                {
+                    Marca marca = (Marca)cboMarca.Items[i];
+
+                    if (marca.Id == articulo.Marca.Id)
+                    {
+                        cboMarca.SelectedIndex = i;
+                        break;
+                    }
+                }
+                for (int i = 0; i < cboCategoria.Items.Count; i++)
+                {
+                    Categoria categoria = (Categoria)cboCategoria.Items[i];
+
+                    if (categoria.Id == articulo.Categoria.Id)
+                    {
+                        cboCategoria.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
