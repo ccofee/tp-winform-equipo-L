e2b0e0d17d24f9bbf29e91cf9169f09cd4289840
Enzo Sander <enzosanderr@gmail.com>
2026-09-14 23:04:32 -0300

    feat: lista los artículos leyendo de la base de datos

---
+++ TPWinForm_Equipo-L/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Dominio;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+
+    public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = @"SELECT 
+                                        A.Id, 
+                                        A.Codigo, 
+                                        A.Nombre, 
+                                        A.Descripcion AS DescArticulo, 
+                                        A.Precio, 
+                                        A.IdMarca, 
+                                        M.Descripcion AS Marca, 
+                                        A.IdCategoria, 
+                                        C.Descripcion AS Categoria 
+                                    FROM ARTICULOS A 
+                                    LEFT JOIN MARCAS M ON A.IdMarca = M.Id 
+                                    LEFT JOIN CATEGORIAS C ON A.IdCategoria = C.Id";
+
+                datos.SetearConsulta(consulta);
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                        {
+                            Articulo aux = new Articulo();
+
+                            aux.Id = (int)datos.Lector["Id"];
+
+                            //validacion de null para codigo
+                            if (!(datos.Lector["Codigo"] is DBNull))
+                                aux.Codigo = (string)datos.Lector["Codigo"];
+                            else
+                                aux.Codigo = string.Empty;
+
+                            //validacion de nombre
+                            if (!(datos.Lector["Nombre"] is DBNull))
+                                aux.Nombre = (string)datos.Lector["Nombre"];
+                            else
+                                aux.Nombre = string.Empty;
+
+                            //validacion de descripcion
+                            if (!(datos.Lector["DescArticulo"] is DBNull))
+                                aux.Descripcion = (string)datos.Lector["DescArticulo"];
+                            else
+                                aux.Descripcion = string.Empty;
+
+                            //validacion de precio
+                            if (!(datos.Lector["Precio"] is DBNull))
+                                aux.Precio = (decimal)datos.Lector["Precio"];
+                            else
+                                aux.Precio = 0;
+
+                            //validacion de MARCA
+                            aux.Marca = new Marca();
+
+                            if (!(datos.Lector["IdMarca"] is DBNull))
+                            {
+                                aux.Marca.Id = (int)datos.Lector["IdMarca"];
+
+                                if (!(datos.Lector["Marca"] is DBNull))
+                                    aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+                                else
+                                    aux.Marca.Descripcion = "Sin Marca";
+                            }
+                            else
+                            {
+                                aux.Marca.Descripcion = "Sin Marca";
+                            }
+
+                            //validacion de CATEGORIA
+                            aux.Categoria = new Categoria();
+
+                            if (!(datos.Lector["IdCategoria"] is DBNull))
+                            {
+                                aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+
+                                if (!(datos.Lector["Categoria"] is DBNull))
+                                    aux.Categoria.Descripcion = (string)datos.Lector["Categoria"];
+                                else
+                                    aux.Categoria.Descripcion = "Sin Categoría";
+                            }
+                            else
+                            {
+                                aux.Categoria.Descripcion = "Sin Categoría";
+                            }
+
+                        lista.Add(aux);
+                    }
+
+                    return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+
+    }
+}
--- TPWinForm_Equipo-L/Presentacion/frmPrincipal.cs
+++ TPWinForm_Equipo-L/Presentacion/frmPrincipal.cs
@@ -8,12 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Dominio;
+using Negocio;
 
 namespace Presentacion
 {
     public partial class frmPrincipal : Form
     {
-        private List<Articulo> listaArticulos;
+        //private List<Articulo> listaArticulos;
 
         public frmPrincipal()
         {
@@ -22,26 +23,18 @@ namespace Presentacion
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            listaArticulos = new List<Articulo>();
+            ArticuloNegocio negocio = new ArticuloNegocio();
 
-            Articulo art1 = new Articulo();
-            art1.Codigo = "A01";
-            art1.Nombre = "Televisor Samsung 50";
-            art1.Descripcion = "Smart TV 4K UHD";
-            art1.Precio = 450000;
+            try
+            {
+                dgvArticulos.DataSource = negocio.listar();
 
-            Articulo art2 = new Articulo();
-            art2.Codigo = "A02";
-            art2.Nombre = "Mouse Logitech G305";
-            art2.Descripcion = "Mouse inalámbrico gamer";
-            art2.Precio = 45000;
-
-            listaArticulos.Add(art1);
-            listaArticulos.Add(art2);
-
-            dgvArticulos.DataSource = listaArticulos;
-
-            dgvArticulos.Columns["Id"].Visible = false;
+                dgvArticulos.Columns["Id"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los artículos: " + ex.Message);
+            }
         }
 
         private void btnVerDetalle_Click(object sender, EventArgs e)
